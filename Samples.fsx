9ce9320734b859f6fb4203e437c60f65f870b839
cboudereau <cboudereau@gmail.com>
2015-08-21 20:37:23 +0200

    init

---
+++ Samples.fsx
@@ -0,0 +1,15 @@
+﻿#r @"FSharp.ProtoBuf\bin\Debug\FSharp.ProtoBuf.dll"
+
+open FSharp.RegexTypeProvider
+open System.Text.RegularExpressions
+
+type test = RegexProvider< "(?<Name>.*)" >
+
+test.IsMatch("clement")
+
+let t = test()
+
+let r = t.Match("clement")
+
+r.Name
+
